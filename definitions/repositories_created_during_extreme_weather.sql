b2c28a7b39b6e1521da751e7bd662d97ad056bbf
Stanislaw Magierski <admin@stanislawm.altostrat.com>
2024-09-06 05:33:26 +0000

    fix

--- definitions/repositories_created_during_extreme_weather.sql
+++ definitions/repositories_created_during_extreme_weather.sql
@@ -1,7 +1,7 @@
 SELECT
     *
 FROM
-    `dataform-open-source.was_there_extreme_weather`
-    LEFT OUTER JOIN `dataform-open-source.repositories_that_mention_extreme_weather` USING (date)
+    `dataform_extreme_weather.was_there_extreme_weather`
+    LEFT OUTER JOIN `dataform_extreme_weather.repositories_that_mention_extreme_weather` USING (date)
 ORDER BY
     date
